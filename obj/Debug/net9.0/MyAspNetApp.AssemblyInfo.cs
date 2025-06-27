62075c413d88afaed504e7b8a450540ad8e8febf
nandimandalammahesh <nandimandalam_mahesh@ifbglobal.com>
2025-06-27 12:10:25 +0530

    comment

--- obj/Debug/net9.0/MyAspNetApp.AssemblyInfo.cs
+++ obj/Debug/net9.0/MyAspNetApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d612beacbbd408291154444b996427a346558b8")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e109864491e1f6a8b482977cd916e88e58324b6d")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyAspNetApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
